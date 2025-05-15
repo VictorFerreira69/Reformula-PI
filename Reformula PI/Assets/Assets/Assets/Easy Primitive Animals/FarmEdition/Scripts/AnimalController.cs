18be59c3c2199f4e4babfb2a99212267650953b0
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-05-15 11:38:27 -0300

    mudando a cena demo

--- Reformula PI/Assets/Assets/Assets/Easy Primitive Animals/FarmEdition/Scripts/AnimalController.cs
+++ Reformula PI/Assets/Assets/Assets/Easy Primitive Animals/FarmEdition/Scripts/AnimalController.cs
@@ -27,7 +27,7 @@ namespace EasyPrimitiveAnimals
         private bool canRotate = true;
         private bool canPeck = true;
 
-        private void Start()
+      /*  private void Start()
         {
             if (!this.gameObject.CompareTag("Chicken"))
             {
@@ -69,7 +69,7 @@ namespace EasyPrimitiveAnimals
                     StartCoroutine(TimeToPeck());
                 }
             }
-        }
+        }*/
 
         private void OnCollisionEnter(Collision collision)
         {
